05fc51211f6ab0dc05c23064acb916d2e5370368
Anoban <99625466+anoban@users.noreply.github.com>
2023-12-02 18:38:31 +0530

    Update python.c

--- python/python.c
+++ python/python.c
@@ -11,8 +11,8 @@ int main(int argc, char* argv[]) {
     srand(time(NULL));
     int minor = 0, major = 0;
 
-    minor = (rand() % 5) + 5;
-    major = (rand() % 9) + 1;
+    major = (rand() % 5) + 5;
+    minor = (rand() % 9) + 1;
 
     if (!strcmp(argv[1], "--version")) printf_s("Python 3.%d.%d\n", major, minor);
 
